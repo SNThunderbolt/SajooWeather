c5b210f04fe9cf82bced1cd6b4ce6dfe7c2aac2d
SajedeNouri <Sajoo@Sajoo-MacBook-Air.local>
2017-08-07 12:17:37 +0430

    Initial Commit

---
+++ SajooWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SajooWeather
+//
+//  Created by SajedeNouri on 5/16/1396 AP.
+//  Copyright © 1396 AP SajedeNouri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
